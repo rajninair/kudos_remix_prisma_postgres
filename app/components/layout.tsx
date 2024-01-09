a18d4470539dd049996a4e3653f0546adb58dddf
rajni.algorisys <rajni.algorisys@ext.idfy.com>
2024-01-09 16:13:22 +0530

    added layout, new component for formfield,  a login page with signup feature

---
+++ app/components/layout.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-screen w-full bg-blue-600 font-mono">{children}</div>
+  );
+};
+export default Layout;
